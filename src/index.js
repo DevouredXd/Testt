f14098ca5cce70357984c3ad77a29021e9377973
DevouredXd <mccjhon9@gmail.com>
2025-04-01 21:19:38 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,54 +1,68 @@
-// index.js
 import { after } from "@vendetta/patcher";
 import { findByProps } from "@vendetta/metro";
 import { React } from "@vendetta/metro/common";
-import { showToast } from "@vendetta/ui/toasts";
 
 export default {
-  onLoad() {
-    const patches = [];
-    
-    // 1. Patch Avatars
-    const Avatar = findByProps("Avatar").Avatar;
-    patches.push(after("default", Avatar, (_, component) => {
-      const uri = component.props.src?.replace(/\?size=\d+/, "?size=4096");
-      if (!uri) return component;
-      
-      return React.createElement(
-        findByProps("TouchableOpacity").TouchableOpacity,
-        {
-          onPress: () => showToast("Opening avatar", { source: { uri } }),
-          activeOpacity: 0.8
-        },
-        component
-      );
-    }));
+    onLoad() {
+        const patches = [];
+        const { TouchableOpacity } = findByProps("TouchableOpacity");
+        const { showImagePreview } = findByProps("showImagePreview");
 
-    // 2. Patch Banners
-    const ProfileCard = findByProps("ProfileCard").ProfileCard;
-    patches.push(after("default", ProfileCard, (_, component) => {
-      const banner = component.props.bannerSource?.uri?.replace(/\?width=\d+/, "?width=1600");
-      if (!banner) return component;
-      
-      return {
-        ...component,
-        props: {
-          ...component.props,
-          children: [
-            React.createElement(
-              findByProps("TouchableOpacity").TouchableOpacity,
-              {
-                onPress: () => showToast("Opening banner", { source: { uri: banner } }),
-                activeOpacity: 0.8
-              },
-              component.props.children[0]
-            ),
-            ...component.props.children.slice(1)
-          ]
+        // 1. Patch Profile Pictures
+        const Avatar = findByProps("Avatar").Avatar;
+        patches.push(after("default", Avatar, ([props], component) => {
+            const highResAvatar = props.src?.replace(/\?size=\d+/, "?size=4096");
+            if (!highResAvatar) return component;
+
+            return React.createElement(
+                TouchableOpacity,
+                {
+                    onPress: () => showImagePreview(highResAvatar, []),
+                    activeOpacity: 0.8
+                },
+                component
+            );
+        }));
+
+        // 2. Patch Banners
+        const ProfileBanner = findByProps("ProfileBanner")?.ProfileBanner;
+        if (ProfileBanner) {
+            patches.push(after("default", ProfileBanner, ([props], component) => {
+                const highResBanner = props.bannerSource?.uri?.replace(/\?width=\d+/, "?width=2048");
+                if (!highResBanner) return component;
+
+                return React.createElement(
+                    TouchableOpacity,
+                    {
+                        onPress: () => showImagePreview(highResBanner, []),
+                        activeOpacity: 0.8
+                    },
+                    component
+                );
+            }));
         }
-      };
-    }));
 
-    return () => patches.forEach(p => p());
-  }
+        // 3. Patch User Profile Header
+        const UserProfileHeader = findByProps("UserProfileHeader")?.UserProfileHeader;
+        if (UserProfileHeader) {
+            patches.push(after("default", UserProfileHeader, (_, component) => {
+                const avatar = component.props?.user?.getAvatarURL?.();
+                const banner = component.props?.bannerSource?.uri;
+
+                return React.createElement(
+                    TouchableOpacity,
+                    {
+                        onPress: () => {
+                            if (banner) showImagePreview(banner.replace(/\?width=\d+/, "?width=2048"), []);
+                            else if (avatar) showImagePreview(avatar.replace(/\?size=\d+/, "?size=4096"), []);
+                        },
+                        activeOpacity: 0.9
+                    },
+                    component
+                );
+            }));
+        }
+
+        return () => patches.forEach(p => p?.());
+    }
 };
